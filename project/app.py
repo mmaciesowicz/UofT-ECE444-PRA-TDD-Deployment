f169953bae4334b67f2e7d0fdf94223bf4b2bbb5
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-10-02 00:15:30 -0400

    changed sql database url

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
 import sqlite3
 from pathlib import Path
 from functools import wraps
+import os
 
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
@@ -14,8 +15,13 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = url
 
 
 # create and initialize a new Flask app
